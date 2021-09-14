5b8c92cec2dc857a29a24ae85d9d8af148f1a09e
Syama Sundar Rangapuram <r.shyamsundar@gmail.com>
2021-09-14 17:46:40 +0200

    Rebasing
    R: Add Python wrapper for calling R's hierarchical methods

--- test/model/r_forecast/test_r_hierarchical_predictor.py
+++ test/model/r_forecast/test_r_hierarchical_predictor.py
@@ -14,17 +14,16 @@
 import pytest
 
 from gluonts.core import serde
-from gluonts.dataset.repository import datasets
-from gluonts.dataset.util import forecast_start
-from gluonts.evaluation import Evaluator, backtest_metrics
-from gluonts.model.forecast import SampleForecast, QuantileForecast
+from gluonts.evaluation import MultivariateEvaluator, backtest_metrics
+from gluonts.model.forecast import SampleForecast
 from gluonts.model.r_forecast import (
-    RUnivariateForecastPredictor,
+    RHierarchicalForecastPredictor,
     R_IS_INSTALLED,
     RPY2_IS_INSTALLED,
-    UNIVARIATE_QUANTILE_FORECAST_METHODS,
-    SUPPORTED_UNIVARIATE_METHODS,
+    SUPPORTED_HIERARCHICAL_METHODS,
 )
+from gluonts.support.pandas import forecast_start
+from test.model.deepvar_hierarchical.generate_hierarchical_dataset import sine7
 
 
 # conditionally skip these tests if `R` and `rpy2` are not installed
@@ -33,47 +32,48 @@ if not R_IS_INSTALLED or not RPY2_IS_INSTALLED:
     pytest.skip(msg=skip_message, allow_module_level=True)
 
 
-TOLERANCE = 1e-6
+seq_length = 100
+prediction_length = 5
+train_datasets = sine7(
+    seq_length=seq_length, prediction_length=prediction_length
+)
 
 
-@pytest.mark.parametrize("method_name", SUPPORTED_UNIVARIATE_METHODS)
-def test_forecasts(method_name):
-    if method_name == "mlp":
-        # https://stackoverflow.com/questions/56254321/error-in-ifncol-matrix-rep-argument-is-of-length-zero
-        # https://cran.r-project.org/web/packages/neuralnet/index.html
-        #   published before the bug fix: https://github.com/bips-hb/neuralnet/pull/21
-        # The issue is still open on nnfor package: https://github.com/trnnick/nnfor/issues/8
-        # TODO: look for a workaround.
-        pytest.xfail(
-            "MLP currently does not work because "
-            "the `neuralnet` package is not yet updated with a known bug fix in ` bips-hb/neuralnet`"
-        )
+TOLERANCE = 1.0
+SUPPORTED_BASE_FORECAST_METHODS = ["ets", "arima"]
 
-    dataset = datasets.get_dataset("constant")
+
+@pytest.mark.parametrize("method_name", SUPPORTED_HIERARCHICAL_METHODS)
+@pytest.mark.parametrize("fmethod", SUPPORTED_BASE_FORECAST_METHODS)
+def test_forecasts(method_name, fmethod):
 
     (train_dataset, test_dataset, metadata) = (
-        dataset.train,
-        dataset.test,
-        dataset.metadata,
+        train_datasets.train,
+        train_datasets.test,
+        train_datasets.metadata,
     )
 
     freq = metadata.freq
-    prediction_length = metadata.prediction_length
+    nodes = metadata.nodes
+    target_dim, num_bottom_ts = train_datasets.metadata.S.shape
 
     params = dict(
-        freq=freq, prediction_length=prediction_length, method_name=method_name
+        freq=freq,
+        prediction_length=prediction_length,
+        is_hts=True,
+        target_dim=target_dim,
+        num_bottom_ts=num_bottom_ts,
+        nodes=nodes,
+        nonnegative=False,
+        method_name=method_name,
+        fmethod=fmethod,
     )
 
-    predictor = RUnivariateForecastPredictor(**params)
+    predictor = RHierarchicalForecastPredictor(**params)
     predictions = list(predictor.predict(train_dataset))
 
-    forecast_type = (
-        QuantileForecast
-        if method_name in UNIVARIATE_QUANTILE_FORECAST_METHODS
-        else SampleForecast
-    )
     assert all(
-        isinstance(prediction, forecast_type) for prediction in predictions
+        isinstance(prediction, SampleForecast) for prediction in predictions
     )
 
     assert all(prediction.freq == freq for prediction in predictions)
@@ -88,11 +88,12 @@ def test_forecasts(method_name):
         for data, prediction in zip(train_dataset, predictions)
     )
 
-    evaluator = Evaluator()
     agg_metrics, item_metrics = backtest_metrics(
         test_dataset=test_dataset,
         predictor=predictor,
-        evaluator=evaluator,
+        evaluator=MultivariateEvaluator(
+            quantiles=(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+        ),
     )
     assert agg_metrics["mean_wQuantileLoss"] < TOLERANCE
     assert agg_metrics["NRMSE"] < TOLERANCE
@@ -100,5 +101,15 @@ def test_forecasts(method_name):
 
 
 def test_r_predictor_serialization():
-    predictor = RUnivariateForecastPredictor(freq="1D", prediction_length=3)
+    predictor = RHierarchicalForecastPredictor(
+        freq="1D",
+        prediction_length=3,
+        is_hts=True,
+        target_dim=7,
+        num_bottom_ts=4,
+        nodes=[2, [2] * 2],
+        nonnegative=False,
+        method_name=SUPPORTED_HIERARCHICAL_METHODS[0],
+        fmethod="ets",
+    )
     assert predictor == serde.decode(serde.encode(predictor))
